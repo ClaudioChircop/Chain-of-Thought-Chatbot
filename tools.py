c2dcf792edef84dbf68c80d59d616ee3a014c396
ClaudioChircop <129539988+ClaudioChircop@users.noreply.github.com>
2025-03-01 19:17:58 +0100

    Update tools.py

--- tools.py
+++ tools.py
@@ -3,7 +3,7 @@ from crewai_tools import SerperDevTool
 
 
 # Set up Serper.dev API Key
-os.environ["SERPER_API_KEY"] = "f1ae638bfb2f9a1972c55518c5a8ae99f1a01610"
+os.environ["SERPER_API_KEY"] = ""
 
 # 🔹 Define the Web Search Tool
 web_search_tool = SerperDevTool()
